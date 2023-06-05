d81a01cd459f5e93629df563485e7fca29a88371
Prince D Jele <diehard@localhost.localdomain>
2023-06-05 19:08:53 +0200

    second correction to checked project code

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,7 +6,7 @@
 * check_cycle - checks if linked list @list has cycle
 * @list: pointer to head of linked list
 * Return: 1 if true Else 0
-*/ 
+*/
 int check_cycle(listint_t *list)
 {
 	Node* slow = head;
@@ -14,11 +14,11 @@ int check_cycle(listint_t *list)
 
 	while (fast != NULL && fast->next != NULL)
 	{
-	       slow = slow->next;
-	       fast = fast->next->next;
-	       
-	       if (slow == fast)
-	       	return (1);
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		return (1);
 	}
 
 	return (0);
