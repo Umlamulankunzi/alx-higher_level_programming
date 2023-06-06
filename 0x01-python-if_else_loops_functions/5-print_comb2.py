6f059d5db8bdf35c0fe0e8f09cd6128e4dddd01e
Prince D Jele <diehard@localhost.localdomain>
2023-06-06 21:35:56 +0200

    correction: task 6 and 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for val in range(100):
 	if val == 99:
-		print("{val}".format(val=val))
+		print("{val:02d}".format(val=val))
 	else:
-		print("{val}".format(val=val), end=", ")
+		print("{val:02d}".format(val=val), end=", ")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,7 +4,7 @@ for ten in range(10):
         if ten == unit:
             continue
         print("{val:02d}".format(val=(ten * 10) + unit), end="")
-        if (ten * 10) + unit == 99:
+        if (ten * 10) + unit == 89:
             print()
         else:
             print(", ", end="")
