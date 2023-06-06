ec9c8556a7072b2291880dd542c1d8ec13b74fd5
Prince D Jele <diehard@localhost.localdomain>
2023-06-06 21:37:17 +0200

    correction: task 5: remove tabs

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for val in range(100):
-	if val == 99:
-		print("{val:02d}".format(val=val))
-	else:
-		print("{val:02d}".format(val=val), end=", ")
+    if val == 99:
+        print("{val:02d}".format(val=val))
+    else:
+        print("{val:02d}".format(val=val), end=", ")
