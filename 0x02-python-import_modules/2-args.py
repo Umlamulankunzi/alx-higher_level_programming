d9f3907ee217140ee80f553cd1b32eed1ac59e8a
Prince D Jele <diehard@localhost.localdomain>
2023-06-08 12:13:01 +0200

    first upload tasks: 0 to 2
    Finished coding tasks 0 to 2.

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def print_args():
+    num_args = len(sys.argv)
+
+    if num_args == 1:
+        print("0 arguments.")
+        return
+
+    text = "arguments" if num_args - 1 == 1 else "arguments"
+    print(f"{num_args - 1} {text}")
+
+    for ser, arg in enumerate(sys.argv[1:], 1):
+        print(f"{ser}: {arg}")
+
+
+if __name__ == "__main__":
+    print_args()
