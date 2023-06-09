fd0f6074efa7684655be7b3d7700981f63106e96
root <studentjele@gmail.com>
2023-06-08 18:41:40 -0700

    corrected task 4: spelling of keyword

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,7 @@ from hidden_4 import *
 
 
 if __name__ == "__main__":
-    var_arr = sorted(ir())
+    var_arr = dir()
     for index in range(0, len(var_arr)):
         if var_arr[index][0:2] != "__":
             print("{}".format(var_arr[index]))
