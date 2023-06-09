063f052da6a8ab70a9768ff146e63dcd8184732d
root <studentjele@gmail.com>
2023-06-08 18:19:02 -0700

    correction task 4 & 5
    corrected file name task 5 & corrected var_arr variable name task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,6 @@ ftom hidden_4 import *
 
 if __name__ == "__main__":
     var_arr = dir()
-    for index in range(0, len(arr)):
+    for index in range(0, len(var_arr)):
         if var_arr[index][0:2] != "__":
             print("{}".format(var_arr[index]))
