97f33b5385c48357f7dff132f857854fab085a03
Prince D Jele <31880807+Umlamulankunzi@users.noreply.github.com>
2023-06-09 03:05:51 +0200

    finished task 3 to 5
    finished initial coding of task 3 to 5

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+
+
+if __name__ == "__main__":
+    sum = 0
+    for index in range(1, len(sys.argv)):
+        sum += int(sys.argv[index])
+    print("{}".format(sum))
---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+ftom hidden_4 import *
+
+
+if __name__ == "__main__":
+    var_arr = dir()
+    for index in range(0, len(arr)):
+        if var_arr[index][0:2] != "__":
+            print("{}".format(var_arr[index]))
