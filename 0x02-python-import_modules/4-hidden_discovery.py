0da528c01d5b6054f44b6d50e7162717138297aa
root <studentjele@gmail.com>
2023-06-08 18:25:00 -0700

    correction task 4
    sorted var_arr to enable printing variable in sorted order

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,7 @@ ftom hidden_4 import *
 
 
 if __name__ == "__main__":
-    var_arr = dir()
+    var_arr = sorted(ir())
     for index in range(0, len(var_arr)):
         if var_arr[index][0:2] != "__":
             print("{}".format(var_arr[index]))
