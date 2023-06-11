2fbb62f7bd83a1deebbe3d46e7dab8fa9aab854b
Prince D Jele <diehard@localhost.localdomain>
2023-06-11 15:13:54 +0200

    correction: task 6
    correction the formatting when printing the list elements

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for sub_list in matrix:
         len_sublst = len(sub_list)
         for index in range(len_sublst):
-            print("{}".format(sub_list[index]), end="")
+            print("{:d}".format(sub_list[index]), end="")
 
             if index == len_sublst - 1:
                 print()
