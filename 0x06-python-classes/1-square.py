1adbce9470d2d30109aede70a95c09d6a8e44d16
Prince D Jele <diehard@localhost.localdomain>
2023-06-27 21:20:11 +0200

    added module level doc strings

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+
+"""
+This module provides a class blueprint for a Square object.
+"""
+
+
 class Square:
     """class implementing square object
     """
--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+
+"""
+This module provides a class blueprint for a MagicClass object.
+"""
+
+
 import math
 
 
