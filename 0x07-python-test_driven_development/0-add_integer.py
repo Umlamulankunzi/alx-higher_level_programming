d8c8319e422badabdf2b7fbd855b0fe8bcafb76f
Prince D Jele <diehard@localhost.localdomain>
2023-07-02 11:54:21 +0200

    created directory and Readme

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+"""
+Module defining an add function that adds two numbers together
+
+
+"""
+
+def add_integer(a, b=98):
+    """adds two numbers together
+    Returns: int
+    """
+    if not isinstance(a, (float, int)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (float, int)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
