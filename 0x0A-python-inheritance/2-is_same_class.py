3b23df44e927879a4cf74c73d6a1d1b5fc34dc17
root <studentjele@gmail.com>
2023-07-10 14:16:57 -0700

    correct mispelt keyword def in task 2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -8,7 +8,7 @@ Otherwise False
 """
 
 
-pdef is_same_class(obj, a_class):
+def is_same_class(obj, a_class):
     """Returns True if the object is exactly an instance of the,
     specified class ; otherwise False
 
