41066b120bf90dce41fe6746ab011fda3d12a6e1
Prince D Jele <31880807+Umlamulankunzi@users.noreply.github.com>
2023-08-17 22:05:00 +0200

    Update 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,8 +1,8 @@
 -- Lists all shows contained in the database hbtn_0d_tvshows.
 -- Display NULL for shows without genres.
 -- Results ordered by tv_shows.title and tv_show_genres.genre_id in ASC order
-SELECT show.'title', genre.'genre_id'
-FROM 'tv_shows' AS show
-LEFT JOIN 'tv_show_genres' AS genre
-ON show.'id' = genre.'show_id'
-ORDER BY show.'title', genre.'genre_id';
+SELECT show.title, genre.genre_id
+FROM tv_shows AS show
+LEFT JOIN tv_show_genres AS genre
+ON show.id = genre.show_id
+ORDER BY show.title, genre.genre_id;
