aa47465ab0cf9f3ef86da8db7618bae6cdca321e
Prince D Jele <31880807+Umlamulankunzi@users.noreply.github.com>
2023-08-17 22:03:33 +0200

    Update 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,7 @@
 -- Lists all shows in hbtn_0d_tvshows which have at least one genre linked.
 -- Records odered by tv_shows.title and tv_show_genres.genre_id.
-SELECT show.'title', genre.'genre_id'
-FROM 'tv_shows' AS show
-    INNER JOIN 'tv_show_genres' AS genre
-    ON show.'id' = genre.'show_id'
-ORDER BY show.'title', genre.'genre_id';
+SELECT show.title, genre.genre_id
+FROM tv_shows AS show
+    INNER JOIN tv_show_genres AS genre
+    ON show.id = genre.show_id
+ORDER BY show.title, genre.genre_id;
