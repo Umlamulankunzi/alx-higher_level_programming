ef5ddba7d784e6aaeebbd38d980e93438417d9ca
Prince D Jele <31880807+Umlamulankunzi@users.noreply.github.com>
2023-09-12 21:30:31 +0200

    Completed mandatory tasks 0 to 6

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/**
+ * Rectangle class with width and height attributes
+ */
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
